d4a84809d78f59656eda5b4f73c8c47d5fee2441
fazekasjulcsi <julcsi.fazekas14@gmail.com>
2024-09-30 21:36:00 +0200

    sstage_5

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function getUsers() {
-    fetch("https://randomuser.me/api/?format=json")
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  }
-  
-getUsers();
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(data => {
+      const userProfilePicture = data.results[0].picture.large;
+      document.getElementById('random-profile').src = userProfilePicture;
+  })
+  .catch(error => console.error('Error fetching random user profile:', error));
+});
